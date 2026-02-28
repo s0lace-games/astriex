8b6790aeb6346de6acd1d0ae4745c1324301201e
grant <grantschmidt8@gmail.com>
2026-02-28 10:05:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import { createServer } from "http";
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
-const bare = createBareServer("/", {
-  logErrors: true,
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.static(join(__dirname, "public")));
+
+app.use("*", (_req, res) => {
+  res.sendFile(join(__dirname, "public/index.html"));
 });
 
-const server = createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-
-  if (req.method === "OPTIONS") {
-    res.writeHead(204);
-    res.end();
-    return;
-  }
-
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ status: "ok", server: "bare" }));
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.destroy();
-  }
-});
-
-server.on("error", (err) => console.error("Server error:", err));
-
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, "0.0.0.0", () => console.log("Bare server on port", PORT));
+export default app;
